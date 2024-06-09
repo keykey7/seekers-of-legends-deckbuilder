3d6313edd665bfc5c87fbe9a4359dfbb2249bc7f
keykey7 <29210485+keykey7@users.noreply.github.com>
2024-06-09 20:17:15 +0200

    anime js working with static coordinates

--- src/ParticleAnimation.tsx
+++ src/ParticleAnimation.tsx
@@ -1,46 +1,64 @@
-// @ts-expect-error TS7016: Could not find a declaration file for module react-anime.
-import Anime, {anime} from 'react-anime';
-import React, {ReactElement} from 'react';
+import anime from "animejs";
+import {ReactElement, useEffect, useRef} from 'react';
 import styles from './ParticleAnimation.module.css';
 
 function ParticleAnimation() {
+  const ref = useRef(null);
+  useEffect(() => {
+    const animeTargets = document.querySelectorAll("." + styles.dot);
+    const stagger = anime.stagger(1, {easing: 'easeInCubic'}); // https://easings.net/
+    const dtMax = 1000;
+    ref.current = anime({
+      targets: animeTargets,
+      loop: false,
+      easing: "linear",
+      opacity: [
+        { value: 1, duration: 100, delay: stagger},
+        { value: 0, duration: function(){return anime.random(500, dtMax);}}
+      ],
+      width: { value: 2, duration: 500, delay: stagger },
+      height: { value: 2, duration: 500, delay: stagger },
+      translateX: {
+        value: function() {
+          return anime.random(-30, 30);
+        },
+        duration: dtMax,
+        delay: stagger,
+      },
+      translateY: {
+        value: function() {
+          return anime.random(-30, 30);
+        },
+        duration: dtMax,
+        delay: stagger,
+      }
+    });
+  }, []);
+
   const elements: ReactElement[] = [];
   for (let i = 0; i < 100; i++) {
     const top = 100 + i *3;
     const left = 100 + i *3;
-    const n = 15;
+    const n = 10;
     for (let j = 0; j < n; j++) {
-      const size = anime.random(5, 10);
-      const Dot = <div key={"anim" + i * n + j} className={styles.dot}
+      const size = anime.random(10, 20);
+      const Dot = <div key={"anim" +(n*i+j)} className={styles.dot}
             style={{
               width: size + "px",
               height: size + "px",
               top: top + anime.random(-15, 15) + "px",
               left: left + anime.random(-15, 15) + "px",
-              //opacity: 0,
+              opacity: 0,
             }}
           />;
       elements.push(Dot);
     }
   }
+  console.log("init component")
   return (
-    <div>
-      <Anime
-        //loop={true} // debug
-        easing={'linear'}
-        opacity={[
-          { value: 1, duration: 50, delay: anime.stagger(2) },
-          { value: 0, duration: function(){return anime.random(500,1500);}}
-        ]}
-        width={{ value: 2, duration: 500, delay: anime.stagger(2) }}
-        height={{ value: 2, duration: 500, delay: anime.stagger(2) }}
-        duration={1500}
-        delay={anime.stagger(2)}
-        // TODO 08-Jun-2024/kk: translate
-      >
-        {elements.map(x => x)}
-      </Anime>
-    </div>
+    <>
+      {elements}
+    </>
   );
 }
 
