1fe7cbdca3d8e1a2037f9702f75e0cb2e014ab16
keykey7 <29210485+keykey7@users.noreply.github.com>
2024-08-08 16:42:49 +0200

    lock icon when enough of type

--- src/compendium/HoverCard.tsx
+++ src/compendium/HoverCard.tsx
@@ -1,19 +1,67 @@
 import {Box, useTheme} from '@mui/material';
 import React from 'react';
-import {Card} from '../Card.tsx';
-import {useDeckDispatch} from '../deck/context/DeckProvider.tsx';
+import {Card, CardType} from '../Card.tsx';
+import {useDeck, useDeckDispatch} from '../deck/context/DeckProvider.tsx';
 import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
+import LockIcon from '@mui/icons-material/Lock';
 import {useIsMobile} from '../MobileUtil.ts';
 
-export interface HoverCardProps {
-  card: Card,
+export interface InfoIconProps {
   onDetailClick: () => void,
+}
+
+export interface HoverCardProps extends InfoIconProps {
+  card: Card,
+  count: number,
+}
+
+function Lock({visible}: Readonly<{ visible: boolean }>) {
+  return <LockIcon sx={{
+    position: 'absolute',
+    left: '50%',
+    transformOrigin: 'center center',
+    transform: ' translate(-50%, 0) scale(2.5)',
+    top: '35%',
+    filter: 'drop-shadow(0 0 2px black)',
+    visibility: visible ? 'initial' : 'hidden',
+  }}/>
+}
+
+function InfoIcon({onDetailClick}: Readonly<InfoIconProps>) {
+  return <InfoTwoToneIcon
+    fontSize="large"
+    onMouseDown={e => {
+      onDetailClick();
+      e.stopPropagation();
+    }}
+    className="infoicon"
+    sx={theme => ({
+      position: 'absolute',
+      right: 0,
+      transform: 'scale(1.8)',
+      filter: 'brightness(85%) drop-shadow(1px 1px 2px rgba(0,0,0,0.5))',
+      ':hover': { // to realize that this is something else when clicked
+        filter: 'brightness(100%) drop-shadow(1px 1px 2px rgba(0,0,0,0.5))',
+      },
+      [theme.breakpoints.down('md')]: { // move slightly into the card on mobile due to tighter space
+        right: 2,
+        top: 1,
+        transform: 'scale(1.6)',
+      },
+      [theme.breakpoints.up('md')]: {
+        opacity: 0,
+        transition: 'opacity 0.2s linear', // fade the info button in on hover
+      },
+    })} />
 }
 
 function HoverCard({card, onDetailClick}: Readonly<HoverCardProps>) {
   const theme = useTheme();
   const cardDispatch = useDeckDispatch();
   const isSmallScreen = useIsMobile();
+  const count = useDeck().countByType(card);
+  const isMaxCount = count === (card.type === CardType.Avatar ? 1 : 4);
+  const maxFilter = isMaxCount ? 'brightness(60%)' : '';
 
   let moveCard = (event: React.MouseEvent<HTMLDivElement>) => {
     const dMax = 15;
@@ -62,41 +110,18 @@ function HoverCard({card, onDetailClick}: Readonly<HoverCardProps>) {
           width: '100%',
           height: '100%',
           backfaceVisibility: 'hidden',
+          filter: maxFilter,
           ':hover': {
-            filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.4))',
-          },
-          '> svg': {
-            filter: 'brightness(85%)',
-          },
-          '> svg:hover': { // to realize that this is something else when clicked
-            filter: 'brightness(100%)',
+            filter: `${maxFilter} drop-shadow(0 0 5px rgba(255, 255, 255, 0.4))`,
           },
           [theme.breakpoints.up('md')]: { // hide the info icon until hover on non-touch devices
-            '> svg': {
-              opacity: 0,
-              transition: 'opacity 0.2s linear', // fade the info button in on hover
-            },
             ':hover > svg': {
               opacity: 1
             },
           }
         }}>
-        <InfoTwoToneIcon
-          fontSize="large"
-          onMouseDown={e => {
-            onDetailClick();
-            e.stopPropagation();
-          }}
-          sx={{
-            position: 'absolute',
-            right: 0,
-            transform: 'scale(1.8)',
-            [theme.breakpoints.down('md')]: { // move slightly into the card on mobile due to tighter space
-              right: 2,
-              top: 1,
-              transform: 'scale(1.6)',
-            }
-          }} />
+        <InfoIcon onDetailClick={onDetailClick} />
+        <Lock visible={isMaxCount} />
       </Box>
     </Box>
   );
