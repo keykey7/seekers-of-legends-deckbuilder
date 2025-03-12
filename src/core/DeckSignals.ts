b129a820bb0306afd7a4ab1c3bb13bb641a4a406
keykey7 <29210485+keykey7@users.noreply.github.com>
2025-03-12 14:01:56 +0100

    avatar quickfilter

--- src/core/DeckSignals.ts
+++ src/core/DeckSignals.ts
@@ -1,8 +1,10 @@
-import {Card} from './Card.ts';
-import {fromUrl, toUrl} from '../deck/StableUrl.ts';
-import {batch, computed, effect, signal} from '@preact/signals';
-import {deckAnimationSignal, Rect} from '../particles/ParticleSignals.ts';
+import { Card, CardType } from './Card.ts';
+import { fromUrl, toUrl } from '../deck/StableUrl.ts';
+import { batch, computed, effect, signal } from '@preact/signals';
+import { deckAnimationSignal, Rect } from '../particles/ParticleSignals.ts';
 import { AvatarAndCards } from './Deck.ts';
+import { filterSignal } from '../compendium/CardFilter.ts';
+import { AVATAR_FILTER_TEXT } from '../deck/DeckAvatar.tsx';
 
 const deckSignal = signal(fromUrl());
 
@@ -23,13 +25,21 @@ export const addCardToDeck = (card: Card, origin: Rect) => {
     return; // noop
   }
   batch(() => {
+    if (deckSignal.value.avatar === undefined && card.type === CardType.Avatar && filterSignal.value.filterText ===
+      AVATAR_FILTER_TEXT) {
+      // if the deck didn't have an avatar yet, and the user clicked on the avatar filter before, we unset that one here
+      filterSignal.value = {
+        ...filterSignal.value,
+        filterText: '',
+      };
+    }
     deckSignal.value = deckSignal.value.withAnyCard(card);
     deckAnimationSignal.value = {
       card,
       origin,
     };
   });
-}
+};
 
 export const removeCardFromDeck = (card: Card, origin: Rect) => {
   batch(() => {
@@ -39,8 +49,8 @@ export const removeCardFromDeck = (card: Card, origin: Rect) => {
       origin,
     };
   });
-}
+};
 
 export const clearDeck = () => {
   deckSignal.value = AvatarAndCards.empty();
-}
+};
--- src/deck/DeckAvatar.tsx
+++ src/deck/DeckAvatar.tsx
@@ -1,18 +1,31 @@
 import DeckItem from './DeckItem.tsx';
-import {Box, Typography} from '@mui/material';
-import {Card} from '../core/Card.ts';
+import { Box, Typography } from '@mui/material';
+import { Card } from '../core/Card.ts';
+import { filterSignal } from '../compendium/CardFilter.ts';
+
+export const AVATAR_FILTER_TEXT = 'Avatar';
 
 /**
  * A special version of a DeckItem representing the Avatar.
  */
 function DeckAvatar({avatar}: Readonly<{avatar: Card | undefined}>) {
+  const onFilterForAvatar = () => {
+    filterSignal.value = {
+      ...filterSignal.value,
+      filterText: AVATAR_FILTER_TEXT,
+    };
+  };
   let element = <Box sx={{
     pt: 2,
     pb: 1,
     textAlign: 'center',
   }}>
     <Typography variant="h5"
-      component="div">
+      component="div"
+      onClick={onFilterForAvatar}
+      sx={{
+        cursor: 'pointer',
+      }}>
       Wähle dein Avatar
     </Typography>
   </Box>;
@@ -25,8 +38,8 @@ function DeckAvatar({avatar}: Readonly<{avatar: Card | undefined}>) {
     </Box>;
   }
   return (<>
-      {element}
-    </>);
+    {element}
+  </>);
 }
 
 export default DeckAvatar;
